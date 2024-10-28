0aeca917d0ed51a021e643560b4060f05c804d2e
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-10-27 23:37:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.querySelectorAll('.card a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        alert("Você está indo para o projeto: " + link.innerText);
-    });
-});
+document.querySelectorAll('.card a') // Seleciona todos os links dentro de elementos com a classe "card" //
+.forEach(link => { // Para cada link encontrado, executa a função seguinte //
+    link.addEventListener('click', (e) => { // Adiciona um evento de clique ao link //
+        alert("Você está indo para o projeto: " + link.innerText); // Exibe um alerta com o texto do link ao ser clicado //
+    }); // Fim da função de evento //
+}); // Fim do método forEach //
